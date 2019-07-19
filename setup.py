78377c9733bd88c9101b85b6d2b230d3a389aa2a
Marin Aleksey <asmadews@gmail.com>
2019-07-19 09:36:58 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+"""Project setup configuration for setuptools"""
+
+from glob import glob
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='sphinx_git_changelog',
+    description='Project Git Changelog for Sphinx',
+    version='0.0.1',
+    author='Aleksey Marin',
+    author_email='asmadews@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[
+        path.splitext(path.basename(path))[0] for path in glob('src/*.py')
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    data_files=[],
+    entry_points={
+        "console_scripts": [
+            "dj_generate=sphinx_dj_generator.cmdline:generate",
+        ]
+    },
+    tests_require=["pytest"],
+    install_requires=[
+        'six',
+        'sphinx',
+        'GitPython>=0.3.6',
+        'setuptools >= 36.0.1'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Console',
+        'Environment :: Plugins',
+        'Framework :: Sphinx',
+        'Framework :: Sphinx :: Extension',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Other Audience',
+        'License :: Other/Proprietary License',
+        'Natural Language :: English',
+        'Natural Language :: Russian',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Documentation :: Sphinx',
+        'Topic :: Software Development :: Documentation',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
