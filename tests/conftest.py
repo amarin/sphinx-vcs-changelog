b165feea4861418f8bce1a7ee9bb9433d43adc10
Marin Aleksey <asmadews@gmail.com>
2019-07-21 16:32:03 +0300

    dev: Added directive_factory to use in tests & command line

---
+++ tests/conftest.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from os import path
+
+from docutils.frontend import Values
+
+from sphinx_vcs_changelog.factory import directive_factory
+
+__docformat__ = 'reStructuredText'
+
+import pytest
+
+
+@pytest.fixture
+def temp_dir():
+    """Make temporary path for testing"""
+    from shutil import rmtree
+    from tempfile import mkdtemp
+    root = mkdtemp()
+    assert path.exists(root)
+    yield root
+    rmtree(root)
+
+
+@pytest.fixture
+def repo(temp_dir):
+    """Make test repo in temporary path"""
+    assert path.exists(temp_dir)
+    from git import Repo
+    username = 'testuser'
+    _repo = Repo.init(temp_dir, mkdir=True)
+    config_writer = _repo.config_writer()
+    config_writer.set_value('user', 'name', username)
+    config_writer.release()
+    return _repo
+
+
+@pytest.fixture
+def test_object_of_class(repo):
+    yield directive_factory(path.join(repo.git_dir, 'changelog.rst'))
