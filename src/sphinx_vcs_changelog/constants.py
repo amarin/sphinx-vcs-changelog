c19f663eb83c4323628bb1657a45738d050ff7d7
Marin Aleksey <asmadews@gmail.com>
2019-07-21 16:12:33 +0300

    ref: added constants descriptive comments

--- src/sphinx_vcs_changelog/constants.py
+++ src/sphinx_vcs_changelog/constants.py
@@ -1,11 +1,42 @@
 # -*- coding: utf-8 -*-
+"""Constants to use internally"""
 
-DIRECTIVE_CHANGELOG_GROUPED = 'changelog'
+#: directive name
+DIRECTIVE_CHANGELOG = 'vcs_changelog'
 
-
+#: option name to setup repo-path
 OPTION_REPO_DIR = 'repo-path'
-OPTION_SINCE = 'added-since'
-OPTION_MAX_RESULTS_COUNT = 'max-count'
-OPTION_MATCH = 'match-regex'
 
-NOTSET = id(__name__)
+#: option name to select commits only after specified commit message or sha
+OPTION_SINCE = 'added-since'
+
+#: option name to take limited results count
+OPTION_MAX_RESULTS_COUNT = 'limit'
+
+#: option name to take commits if message matched regexp only
+OPTION_MATCH = 'filter-regex'
+
+#: option name to count commits if message matched regexp only
+OPTION_MATCH_COUNT = 'context-regex-count'
+
+#: option name to match regexp groups in commits
+OPTION_MATCH_GROUPS = 'context-regex-groups'
+
+#: option to define commit output template
+OPTION_ITEM_TEMPLATE = 'item-template'
+
+#: default commit output template is list item containing summary only
+DEFAULT_ITEM_TEMPLATE = '- {summary}'
+
+#: NOTSET value
+NOTSET = object()
+
+#: NOTSET str representation
+NOTSET_STR = 'empty'
+
+#: Known commit keys to use in templates
+COMMIT_KEYS = [
+    'message',
+    'date',
+    'author',
+]
