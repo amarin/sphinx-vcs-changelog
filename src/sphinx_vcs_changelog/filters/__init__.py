bea9b91552caae1e1e82964075769e748d93a619
Marin Aleksey <asmadews@gmail.com>
2019-08-21 16:04:28 +0300

    dev: Added context extender class

--- src/sphinx_vcs_changelog/filters/__init__.py
+++ src/sphinx_vcs_changelog/filters/__init__.py
@@ -15,8 +15,7 @@
 #  #
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
+import re
 from abc import ABC
 from abc import abstractmethod
 
@@ -58,11 +57,14 @@ class CommitsFilter(object):
 
 
 class OptionFilter(CommitsFilter, ABC):
-    """Filter based on option presence and value"""
+    """Filter based on option presence and value.
+    Implementation can use  regexp as option value, in such case class can
+    have is_regexp = True to automate some checks"""
     option = None
 
     # initial value to compare with
     compare = None
+    is_regexp = False
 
     def __init__(self, directive):
         """Init filter, using specified option name and value
@@ -77,8 +79,82 @@ class OptionFilter(CommitsFilter, ABC):
 
     @property
     def required(self):
-        """Check if this filter suitable.
+        """Check if this filter i.e option value suitable.
 
-        Return False if no option value set
+        Will return:
+        - False if no option value set
+        - False if value is not valid regexp (if have is_regexp=True)
+        - True in all other cases
+
+        Inheritors can overwrite implementation to provide additional checks
         """
-        return self.value != NOTSET
+        is_set = self.value != NOTSET
+        if not is_set:
+            self.directive.info(
+                "Exclude %s as %s not set" % (
+                    self.__class__.__name__,
+                    self.option
+                )
+            )
+            return False
+        if not self.is_regexp:
+            self.directive.info(
+                "Add %s to filter set" % self.__class__.__name__)
+            return True
+
+        try:
+            compiled = self.regexp
+            self.directive.info(
+                "Add %s to filter set" % self.__class__.__name__)
+            return True
+
+        except re.error as exc:
+            self.directive.error(
+                "Exclude %s as %s regexp %s invalid: %s" % (
+                    self.__class__.__name__,
+                    self.option,
+                    self.value,
+                    exc
+                )
+            )
+            return False
+
+    @property
+    def regexp(self):
+        """Lazy compiled regular expression.
+
+        :return: RegexObject"""
+        if not self.is_regexp:
+            raise NotImplementedError(
+                "%s.is_regexp=False" % self.__class__.__name__
+            )
+        store_in = '__regexp'
+        if not hasattr(self, store_in):
+            setattr(self, store_in, re.compile(self.value))
+        compiled_regexp = getattr(self, store_in)
+        return compiled_regexp
+
+
+class ContextExtender(OptionFilter):
+    """Filter not filtering anything, but extending result data with
+    additional information, extracted from commit messages"""
+
+    def __init__(self, directive):
+        """Init filter, using specified option name and value
+
+        Add internal context to fill into template"""
+        super(ContextExtender, self).__init__(directive)
+        self.context = {}
+
+    def pass_to_render(self, commit):
+        """Dont filter anything"""
+        self.collect_commit_info(commit)
+        return True
+
+    @abstractmethod
+    def collect_commit_info(self, commit):
+        """Analyze commit & collect commit information"""
+        pass
+
+
+
